891af52a87a8ef58d86d853723dffb52abfc720a
Nick Hvattum <nhvattum@gmail.com>
2020-08-15 10:08:59 -0700

    trying to connect front and backend

---
+++ components/Login.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import { Redirect } from 'react-router-dom'
+import setAuthToken from '../utils/setAuthToken'
+
+
+export default function Login(props) {
+    // console logs for testing
+    console.log("🏎 Login Component Rendered")
+    console.log(props)
+
+    let [email, setEmail] = useState("")
+    let [password, setPassword] = useState("")
+
+    let handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    let handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+    
+    let handleSubmit = (e) => {
+        e.preventDefault();
+
+        // capture userData
+        const userData = {
+            email: email,
+            password: password,
+        }
+
+        // make a post request to our API to see check user Authentication
+        axios.post(`${process.env.REACT_APP_API}/api/users/login`, userData)
+            .then(res => {
+                // take res data and set to token
+                 const { token } = res.data
+
+                // save token to localStorage
+                localStorage.setItem('jwtToken', token)
+
+                // set token for Auth Header
+                setAuthToken(token)
+
+                // decode jwt token
+                const decoded = jwt_decode(token)
+
+                // set current user 
+                props.nowCurrentUser(decoded)
+            })
+            .catch(err => console.log(err))
+    }
+
+    if (props.user) return <Redirect to="/profile" user={props.user} />
+   
+    return (
+        <div className="row mt-4">
+        <div className="col-md-7 offset-md-3">
+          <div className="card card-body">
+            <h2 className="py-2">Login</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" value={email} onChange={handleEmail} className="form-control" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" value={password} onChange={handlePassword} className="form-control" required />
+              </div>
+              <button type="submit" className="btn btn-primary float-right">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+}
---
+++ components/Profile.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Profile = (props) => {
+    let userData = props.user
+      ? <div>
+          <h1>Profile</h1>
+            <p><strong>Name:</strong> {props.user.name}</p>
+            <p><strong>email:</strong> {props.user.email}</p>
+            <p><strong>ID:</strong> {props.user.id}</p>
+        </div>
+      : <h4>Loading...</h4>
+
+      let errorDiv = () => {
+        return (
+          <div className="text-center pt-4"><h3>Please <Link to='/login'>login</Link> to view this page</h3></div>
+        )
+        }
+
+      return (
+        <div>
+          {props.user ? userData : errorDiv() }
+        </div>
+      )
+}
+
+export default Profile;
