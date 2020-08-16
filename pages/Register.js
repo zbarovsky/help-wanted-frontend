6682d73191646ac0ee4d633f5f256b0afbfab2ac
Nick Hvattum <nhvattum@gmail.com>
2020-08-15 19:08:00 -0700

    signup works on index, but not on register

---
+++ pages/Register.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState, FormEvent, FunctionComponent } from 'react'
+import Signup from '../components/Signup'
+
+
+const Register = function() {
+    return(
+        <div>
+            <Signup />
+        </div>
+    )
+}
+
+export default Register
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import EditProfile from '../components/editprofile'
+import Signup from '../components/Signup'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Signup />
       <EditProfile />
       
     </div>
