b04de39a7a6625802cd31d4567f37ead32bc8864
Nick Hvattum <nhvattum@gmail.com>
2020-08-15 19:40:33 -0700

    signup route works

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import Signup from '../components/signup'
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import EditProfile from '../components/editprofile'
-import Signup from '../components/Signup'
 
 export default function Home() {
   return (
@@ -10,7 +9,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Signup />
+      {/* <Signup /> */}
       <EditProfile />
       
     </div>
