58010e03babf472e57891f8327c3e626939da8d8
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-08-16 20:11:06 -0500

    bootstrap is being a pain

--- pages/teachercard.tsx
+++ pages/teachercard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
 
 interface UserProps {
     teacher: any // don't have to redeclare the object again
@@ -8,11 +9,29 @@ interface UserProps {
 const TeacherCard: React.FC<UserProps> = ({teacher}) => {
     return (
         <div>
-            <p>{teacher.name}</p>
-            
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                <Card.Body>
+                  <Card.Title>{teacher.name}</Card.Title>
+                  <Card.Text>
+                      {teacher.bio}
+                  </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem> {teacher.instrumentsPlayed} </ListGroupItem>
+                  <ListGroupItem> <p>{teacher.email}</p> </ListGroupItem>
+
+                </ListGroup>
+                <Card.Body>
+                  <Card.Link href="#">Card Link</Card.Link>
+                  <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card> 
         </div>
     );
 };
+
+
 
 
 export default TeacherCard
