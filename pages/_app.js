8d84db42ebf5cd8d2ddae17179231667daddb742
Nick Hvattum <nhvattum@gmail.com>
2020-08-17 14:19:12 -0700

    switching to new branch to incorporate next auth

--- pages/_app.js
+++ pages/_app.js
@@ -22,6 +22,7 @@ function MyApp({ Component, pageProps }) {
     <Component {...pageProps} />
       {/* <Signup /> */}
     </div>
+    
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,105 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import EditProfile from '../components/editprofile'
-import HomeRoute from './profile'
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+// import EditProfile from '../components/editprofile'
+// import HomeRoute from './profile'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+// export default function Home() {
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Create Next App</title>
+//         <link rel="icon" href="/favicon.ico" />
         
-        <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
+//         <script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin></script>
         
-        <script
-          src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-          crossorigin></script>
+//         <script
+//           src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+//           crossorigin></script>
         
-        <script
-          src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-          crossorigin></script>
+//         <script
+//           src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+//           crossorigin></script>
           
-      </Head>
-      <HomeRoute />
-      {/* <EditProfile /> */}
+//       </Head>
+      {/* <HomeRoute /> */}
+
       
-    </div>
-  )
+//     </div>
+//   )
+// }
+
+import React, { useState } from 'react'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import { Redirect } from 'react-router-dom'
+import setAuthToken from '../utils/setAuthToken'
+
+
+export default function Login(props) {
+    // console logs for testing
+    console.log("🏎 Login Component Rendered")
+    console.log(props)
+
+    let [email, setEmail] = useState("")
+    let [password, setPassword] = useState("")
+
+    let handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    let handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+    
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        // capture userData
+        const userData = {
+          email: email,
+          password: password,
+        }
+        
+        // make a post request to our API to see check user Authentication
+        axios.post(`http://localhost:3001/users/login`, userData)
+        .then(res => {
+          // take res data and set to token
+          const { token } = res.data
+          
+          // save token to localStorage
+          localStorage.setItem('jwtToken', token)
+          
+          // set token for Auth Header
+          setAuthToken(token)
+          
+          // decode jwt token
+          const decoded = jwt_decode(token)
+          
+          // set current user 
+          props.nowCurrentUser(decoded)
+          // console.log("props.nowCurrentUser in login.tsx", props.nowCurrentUser(decoded))
+        })
+            .catch(err => console.log(err))
+    }
+
+    if (props.user) return <Redirect to="/profile" currentUser={props.user} />
+    return (
+        <div className="row mt-4">
+        <div className="col-md-7 offset-md-3">
+          <div className="card card-body">
+            <h2 className="py-2">Login</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" value={email} onChange={handleEmail} className="form-control" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" value={password} onChange={handlePassword} className="form-control" required />
+              </div>
+              <button type="submit" className="btn btn-primary float-right">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
 }
