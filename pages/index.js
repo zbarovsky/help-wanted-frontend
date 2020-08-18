acdaf57874f015bbce19f50f72afbb6f8b7e92ad
Nick Hvattum <nhvattum@gmail.com>
2020-08-17 17:21:48 -0700

    landing page has correct content except navbar

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,10 @@ import { Redirect } from 'react-router-dom'
 import setAuthToken from '../utils/setAuthToken'
 import Head from 'next/head'
 import Link from 'next/link'
+import Login from '../components/home'
 
 
-export default function Login(props) {
+export default function LandingPage(props) {
     // console logs for testing
     console.log("🏎 Login Component Rendered")
     console.log(props)
@@ -32,30 +33,8 @@ export default function Login(props) {
           password: password
         }
       }
-        
-        // make a post request to our API to see check user Authentication
-    //     axios.post(`http://localhost:3001/users/login`, userData)
-    //     .then(res => {
-    //       // take res data and set to token
-    //       const { token } = res.data
-          
-    //       // save token to localStorage
-    //       localStorage.setItem('jwtToken', token)
-          
-    //       // set token for Auth Header
-    //       setAuthToken(token)
-          
-    //       // decode jwt token
-    //       const decoded = jwt_decode(token)
-          
-    //       // set current user 
-    //       props.nowCurrentUser(decoded)
-    //       // console.log("props.nowCurrentUser in login.tsx", props.nowCurrentUser(decoded))
-    //     })
-    //         .catch(err => console.log(err))
-    // }
 
-    // if (props.user) return <Redirect to="/profile" currentUser={props.user} />
+
     return (
         <div className="row mt-4">
           <Head>
@@ -72,25 +51,8 @@ export default function Login(props) {
               src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
               crossorigin></script>
           </Head>
-        <div className="col-md-7 offset-md-3">
-          <div className="card card-body">
-            <h2 className="py-2">Login</h2>
-            
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" value={email} onChange={handleEmail} className="form-control" required />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={password} onChange={handlePassword} className="form-control" required />
-              </div>
-              <button type="submit" className="btn btn-primary float-right">
-                <Link href="/viewcards">
-                  <a >Submit</a>
-                </Link>
-              </button>
-          </div>
+          <Login />
+
         </div>
-      </div>
     )
 }
